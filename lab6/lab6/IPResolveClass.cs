8b6b31ada0e22101f397fd572841558ee8e3063b
Ilya Yushkevich <70703046+urutet@users.noreply.github.com>
2020-12-10 22:53:54 +0300

    lab6 done

---
+++ lab6/lab6/IPResolveClass.cs
@@ -0,0 +1,215 @@
+﻿using System;
+namespace lab6
+{
+    public class IPResolveClass
+    {
+        string ipAddress;
+        string IPAddress { get => ipAddress; }
+        string subnetMask;
+        string SubnetMask { get => subnetMask; }
+        string subnetID;
+        string SubnetID { get => subnetID; set => subnetID = value; }
+        string hostID;
+        string HostID { get => hostID; set => hostID = value; }
+        string[] addressInBinary;
+        public string this[int i]
+        {
+            get
+            {
+                return addressInBinary[i];
+            }
+        }
+            
+        public IPResolveClass(string ip, string mask)
+        {
+            ipAddress = ip;
+            subnetMask = mask;
+
+        }
+
+        public bool CheckAddress()
+        {
+            try
+            {
+                string[] addressSplit = IPAddress.Split('.');
+                if (addressSplit.Length == 4)
+                {
+                    foreach (string s in addressSplit)
+                    {
+                        if (System.Convert.ToInt32(s) <= 255 && System.Convert.ToInt32(s) >= 0)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("String is in incorrect format");
+                return false;
+            }
+        }
+
+        public bool CheckMask()
+        {
+            try
+            {
+                string[] maskSplit = SubnetMask.Split('.');
+                if (maskSplit.Length == 4)
+                {
+                    if (maskSplit[0] == "255")
+                    {
+                        if (maskSplit[1] == "255")
+                        {
+                            if (maskSplit[2] == "255")
+                            {
+                                if (maskSplit[3] == "255")
+                                {
+                                    return true;
+                                }
+                                else if (maskSplit[3] == "254" || maskSplit[3] == "252" || maskSplit[3] == "248" || maskSplit[3] == "240" || maskSplit[3] == "224" || maskSplit[3] == "192" || maskSplit[3] == "128" || maskSplit[3] == "0")
+                                {
+                                    return true;
+                                }
+                                else
+                                {
+                                    return false;
+                                }
+                            }
+                            else if (maskSplit[2] == "254" || maskSplit[2] == "252" || maskSplit[2] == "248" || maskSplit[2] == "240" || maskSplit[2] == "224" || maskSplit[2] == "192" || maskSplit[2] == "128" || maskSplit[2] == "0")
+                            {
+                                for (int i = 3; i < maskSplit.Length; i++)
+                                {
+                                    if (maskSplit[i] != "0")
+                                    {
+                                        return false;
+                                    }
+                                }
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                        else if (maskSplit[1] == "254" || maskSplit[1] == "252" || maskSplit[1] == "248" || maskSplit[1] == "240" || maskSplit[1] == "224" || maskSplit[1] == "192" || maskSplit[1] == "128" || maskSplit[1] == "0")
+                        {
+                            for (int i = 2; i < maskSplit.Length; i++)
+                            {
+                                if (maskSplit[i] != "0")
+                                {
+                                    return false;
+                                }
+                            }
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        if (maskSplit[0] == "254" || maskSplit[0] == "252" || maskSplit[0] == "248" || maskSplit[0] == "240" || maskSplit[0] == "224" || maskSplit[0] == "192" || maskSplit[0] == "128" || maskSplit[0] == "0")
+                        {
+                            for (int i = 1; i < maskSplit.Length; i++)
+                            {
+                                if (maskSplit[i] != "0")
+                                {
+                                    return false;
+                                }
+                            }
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("String is in incorrect format");
+                return false;
+            }
+        }
+
+        public string ShowSubnetID()
+        {
+            try
+            {
+                string[] ipAddressSplit = IPAddress.Split('.');
+                string[] subnetMaskSplit = SubnetMask.Split('.');
+
+                uint[] ipAddressInt = new uint[ipAddressSplit.Length];
+                uint[] subnetMaskInt = new uint[subnetMaskSplit.Length];
+                uint[] subnetIDInt = new uint[ipAddressSplit.Length];
+
+                for (int i = 0; i < ipAddressSplit.Length; i++)
+                {
+                    ipAddressInt[i] = Convert.ToUInt32(ipAddressSplit[i]);
+                    subnetMaskInt[i] = Convert.ToUInt32(subnetMaskSplit[i]);
+                }
+
+                for (int i = 0; i < ipAddressInt.Length; i++)
+                {
+                    subnetIDInt[i] = ipAddressInt[i] & subnetMaskInt[i];
+                }
+
+                subnetID = $"{subnetIDInt[0]}.{subnetIDInt[1]}.{subnetIDInt[2]}.{subnetIDInt[3]}";
+                return $"{subnetIDInt[0]}.{subnetIDInt[1]}.{subnetIDInt[2]}.{subnetIDInt[3]}";
+            }
+            catch
+            {
+                Console.WriteLine("String is in incorrect format");
+                return "Fail";
+            }
+        }
+
+        public string ShowHostID()
+        {
+            try
+            {
+                string[] ipAddressSplit = IPAddress.Split('.');
+                string[] subnetMaskSplit = SubnetMask.Split('.');
+
+                uint[] ipAddressInt = new uint[ipAddressSplit.Length];
+                uint[] subnetMaskInt = new uint[subnetMaskSplit.Length];
+                uint[] hostIDInt = new uint[ipAddressSplit.Length];
+
+                for (int i = 0; i < ipAddressSplit.Length; i++)
+                {
+                    ipAddressInt[i] = Convert.ToUInt32(ipAddressSplit[i]);
+                    subnetMaskInt[i] = Convert.ToUInt32(subnetMaskSplit[i]);
+                }
+
+                for (int i = 0; i < ipAddressInt.Length; i++)
+                {
+                    hostIDInt[i] = ipAddressInt[i] & ~subnetMaskInt[i];
+                }
+
+                HostID = $"{hostIDInt[0]}.{hostIDInt[1]}.{hostIDInt[2]}.{hostIDInt[3]}";
+                return $"{hostIDInt[0]}.{hostIDInt[1]}.{hostIDInt[2]}.{hostIDInt[3]}";
+            }
+            catch
+            {
+                Console.WriteLine("String is in incorrect format");
+                return "Fail";
+            }
+        }
+    }
+}
---
+++ lab6/lab6/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Threading;
+namespace lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string k = "10";
+            IPResolveClass addr = null;
+            string ipAddress;
+            string subnetMask;
+            do
+            {
+                Console.WriteLine("Press to continue");
+                Console.ReadKey();
+                Console.Clear();
+                Console.WriteLine("˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜");
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1) Create new pair (IP Address + Subnet Mask)");
+                Console.WriteLine("2) Check Address");
+                Console.WriteLine("3) Check Mask");
+                Console.WriteLine("4) Show SubnetID");
+                Console.WriteLine("5) Show HostID");
+                Console.WriteLine("6) Show SubnetID & HostID");
+                Console.WriteLine("0) Exit");
+                Console.WriteLine("˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜");
+                k = Console.ReadLine();
+
+                switch (k)
+                {
+                    case "1":
+                        addr = null;
+                        Console.WriteLine("Enter IP: ");
+                        ipAddress = Console.ReadLine();
+                        Console.WriteLine("Enter subnet mask: ");
+                        subnetMask = Console.ReadLine();
+                        addr = new IPResolveClass(ipAddress, subnetMask);
+                        break;
+                    case "2":
+                        Console.WriteLine("Address is: ");
+                        Console.WriteLine(addr.CheckAddress());
+                        break;
+                    case "3":
+                        Console.WriteLine("Subnet mask is: ");
+                        Console.WriteLine(addr.CheckMask());
+                        break;
+                    case "4":
+                        if (addr.CheckAddress() == false || addr.CheckMask() == false)
+                        {
+                            Console.WriteLine("Correct your data.");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("SubnetID is: ");
+                            Console.WriteLine(addr.ShowSubnetID());
+                            break;
+                        }
+                    case "5":
+                        if (addr.CheckAddress() == false || addr.CheckMask() == false)
+                        {
+                            Console.WriteLine("Correct your data.");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("HostID is: ");
+                            Console.WriteLine(addr.ShowHostID());
+                            break;
+                        }
+                    case "6":
+                        if (addr.CheckAddress() == false || addr.CheckMask() == false)
+                        {
+                            Console.WriteLine("Correct your data.");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("SubnetID is: ");
+                            Console.WriteLine(addr.ShowSubnetID());
+                            Console.WriteLine("HostID is: ");
+                            Console.WriteLine(addr.ShowHostID());
+                            break;
+                        }
+                }
+            } while (k != "0");
+        }
+    }
+}
